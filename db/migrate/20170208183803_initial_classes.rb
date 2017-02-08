13e4c7c81f4ca576eb41acb473fca190e7c186a0
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2017-02-08 12:46:55 -0700

    migration file

---
+++ db/migrate/20170208183803_initial_classes.rb
@@ -0,0 +1,76 @@
+class InitialClasses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :characters do |t|
+      t.integer :health
+      t.string :name
+      t.running_speed :integer
+      t.crouching_speed :integer
+      t.origin :string
+
+      t.belongs_to :class
+    end
+
+    create_table :classes do |t|
+      t.string :name
+    end
+
+    create_table :abilities do |t|
+      t.float :cooldown
+      t.float :damage_done
+      t.integer :maximum_range
+      t.boolean :is_projectile
+      t.boolean :is_hitscan
+      t.float :projectile_speed
+      t.string :information
+
+      t.belongs_to :character
+    end
+
+    create_table :weapons do |t|
+      t.float :damage_per_second
+      t.float :shots_per_second
+      t.float :damage_per_shot
+      t.integer :capacity
+      t.float :headshot_multiplier
+      t.boolean :is_projectile
+      t.boolean :is_hitscan
+      t.float :projectile_speed
+
+      t.belongs_to :character
+    end
+
+    create_table :ultimates do |t|
+      t.integer :time
+      t.integer :total_damage_done
+      t.float :damage_per_second
+      t.float :headshot_multiplier
+      t.integer :average_charge_rate
+
+      t.belongs_to :character
+    end
+
+    create_table :maps do |t|
+      t.string :name
+      t.string :country
+    end
+
+    create_table :game_types do |t|
+      t.string :name
+      t.string :description
+      t.string :objective_team_1
+      t.string :objective_team_2
+    end
+
+    create_table :character_excells_at_map, id: false do |t|
+      t.references :character
+      t.reference :map
+    end
+    add_index :character_excells_at_map, [:character_id, :map_id]
+
+    create_table :map_game_types, id: false do |t|
+      t.refernces :map
+      t.references :game_type
+    end
+    add_index :map_game_types, [:map_id, :game_type_id]
+  end
+end
